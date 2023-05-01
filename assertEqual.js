685cefba89b694b4ebfc9ad1beefd462cfbdb68a
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-01 17:21:19 -0400

    use Template literals instead of operators to concatinate strings in console

--- assertEqual.js
+++ assertEqual.js
@@ -2,10 +2,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     let green = '\u{1F7E2}';
-    return console.log(green + green + green + "    Assertion Passed: " + actual + " === " + expected);
+    return console.log(`${green}${green}${green} Assertion Passed: ${actual}===${expected}`);
   }
   let red = '\u{1F534}';
-  return console.log(red + red + red + "    Assertion Failed: " + actual + " !== " + expected);
+  return console.log(`${red}${red}${red} Assertion Passed: ${actual}===${expected}`);
 };
 
 // TEST CODE
