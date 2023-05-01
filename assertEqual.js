30112d019052eadc5b68c4db5b67d28b8e9c4cf2
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-01 17:03:55 -0400

    assert check with emoji

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    let green = '\u{1F7E2}';
+    return console.log(green + green + green + "    Assertion Passed: " + actual + " === " + expected);
+  }
+  let red = '\u{1F534}';
+  return console.log(red + red + red + "    Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
