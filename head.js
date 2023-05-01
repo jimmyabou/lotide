46c35f2c17b820ce18365b832e7fdae6a9122a3c
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-01 17:30:58 -0400

    cmopare first element of array with assertion function

---
+++ head.js
@@ -0,0 +1,18 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    let green = '\u{1F7E2}';
+    return console.log(`${green}${green}${green} Assertion Passed: ${actual}===${expected}`);
+  }
+  let red = '\u{1F534}';
+  return console.log(`${red}${red}${red} Assertion failed: ${actual}===${expected}`);
+};
+const head= function(array){
+  return array[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// TEST CODE
+//assertEqual("Lighthouse Labs", "Bootcamp");
+//assertEqual(1, 1);
