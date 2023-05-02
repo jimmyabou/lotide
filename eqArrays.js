8ede9cd0b7ad86a66c712b94232426f19a44da78
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-02 19:40:05 -0400

    added file eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    let green = '\u{1F7E2}';
+    return console.log(`${green}${green}${green} Assertion Passed: ${actual}===${expected}`);
+  }
+  let red = '\u{1F534}';
+  return console.log(`${red}${red}${red} Assertion Failed: ${actual}===${expected}`);
+};
+
+const eqArrays=function(firstArray,SecondArray){
+  if (firstArray.length!==SecondArray.length){
+    return false;
+  }
+  for (var i=0;i<firstArray.length;i++){
+    if(firstArray[i]!==SecondArray[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true)
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
--- head.js
+++ head.js
@@ -12,7 +12,7 @@ const head= function(array){
 }
 
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hell");
 // TEST CODE
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual(1, 1);
