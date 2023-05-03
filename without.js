f9b836b66dbcdfd796d6a3e24d59d3d74e3e5cd7
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-02 20:48:38 -0400

    created without.js

--- without.js
+++ without.js
@@ -1,5 +1,5 @@
 const assertArraysEqual=function(first,second){
-  if(eqArrays(first,second)===true){
+  if(eqArrays(first,second)){
     let green = '\u{1F7E2}';
     return console.log(`${green}${green}${green} Assertion Passed: ${JSON.stringify(first)}===${JSON.stringify(second)}`);
   }
@@ -18,3 +18,22 @@ const eqArrays=function(firstArray,SecondArray){
   }
   return true;
 }
+
+const without=function(one,two){
+  let finalArray=one;
+  for (var i=0;i<two.length;i++){
+    for (var y=0;y<finalArray.length;y++){
+      if(one[y]===two[i]){
+      finalArray.splice(y,1)
+  }
+}
+  }return finalArray;
+}
+//console.log(without([1, 2, 3], [1])) // => [2, 3]
+//console.log(without(["1", "2", "3"], [1, 2, "3"]))
+//
+//console.log(without([1, 2, 3], [3, 2, 1]))
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
