5f215b6c72f6d14b78ced5ab7f4760d54c7a21ff
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-08 14:46:08 -0400

    refactored eqArrays.js and created eqArrayTest.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,12 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    let green = '\u{1F7E2}';
-    return console.log(`${green}${green}${green} Assertion Passed: ${actual}===${expected}`);
-  }
-  let red = '\u{1F534}';
-  return console.log(`${red}${red}${red} Assertion Failed: ${actual}===${expected}`);
-};
+//const assertEqual = function(actual, expected) {
+//  if (actual === expected) {
+//    let green = '\u{1F7E2}';
+//    return console.log(`${green}${green}${green} Assertion Passed: ${actual}===${expected}`);
+//  }
+//  let red = '\u{1F534}';
+//  return console.log(`${red}${red}${red} Assertion Failed: ${actual}===${expected}`);
+//};
 
 const eqArrays = function(firstArray,SecondArray) {
   if (firstArray.length !== SecondArray.length) {
@@ -19,11 +19,11 @@ const eqArrays = function(firstArray,SecondArray) {
   }
   return true;
 };
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true)
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports=eqArrays
+//console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+//console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+//
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true)
+//console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+//
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
