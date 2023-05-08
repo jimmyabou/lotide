2e66cd160d4f37aca86a072098538e6474d6532e
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-08 10:19:14 -0400

    refactored head and tail.js in Lotide with adding test for each inside test folder

--- head.js
+++ head.js
@@ -1,18 +1,19 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    let green = '\u{1F7E2}';
-    return console.log(`${green}${green}${green} Assertion Passed: ${actual}===${expected}`);
-  }
-  let red = '\u{1F534}';
-  return console.log(`${red}${red}${red} Assertion failed: ${actual}===${expected}`);
-};
+//const assertEqual = function(actual, expected) {
+//  if (actual === expected) {
+//    let green = '\u{1F7E2}';
+//    return console.log(`${green}${green}${green} Assertion Passed: ${actual}===${expected}`);
+//  }
+//  let red = '\u{1F534}';
+//  return console.log(`${red}${red}${red} Assertion failed: ${actual}===${expected}`);
+//};
+//const assertEqual = require('./assertEqual');
 const head = function(array) {
   return array[0];
 };
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hell");
+module.exports=head;
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hell");
 // TEST CODE
 //assertEqual("Lighthouse Labs", "Bootcamp");
 //assertEqual(1, 1);
