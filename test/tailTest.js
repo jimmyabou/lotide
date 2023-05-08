2ffed431b42c1b3ef6e33ff8071c17bf93811e31
jamilaboudib <jimmy_aboudib@hotmail.com>
2023-05-08 15:33:21 -0400

    head.js modified to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,16 @@
-const assertEqual=require('../assertEqual');
+//const assertEqual=require('../assertEqual');
+//const tail=require('../tail');
+//const words = ["Yo Yo", "Lighthouse", "Labs"];
+//assertEqual(tail(words).length, 2); 
+//assertEqual(words.length, 3);
+const assert = require('chai').assert;
 const tail=require('../tail');
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-assertEqual(tail(words).length, 2); 
-assertEqual(words.length, 3);
+
+describe("#tail", () => {
+  it("returns 3 for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2,3]);
+  });
+  //it("returns '5' for ['5']", () => {
+  //  assert.strictEqual(tail(['5']), '5'); 
+  //});
+});
